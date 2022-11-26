4c275c572e85fce5651ff5d1ee3d78dc271bdf85
Frank Garrote Cruz <javiergc@amazon.com>
2022-11-26 12:22:12 +0100

    check automatic update

--- script/socialweb.js
+++ script/socialweb.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Social Web Tampermonkey Script
 // @namespace   https://github.com/vitrvm/SocialWeb
-// @version     0.1.1
+// @version     0.1.2
 // @description Make the WWW the social web
 // @match       *://*/*
 // @downloadURL https://raw.githubusercontent.com/vitrvm/SocialWeb/main/script/socialweb.js
