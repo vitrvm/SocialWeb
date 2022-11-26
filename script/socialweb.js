617acba9620183712b3cd3bd4798dec0485ff166
Frank Garrote Cruz <javiergc@amazon.com>
2022-11-26 12:21:00 +0100

    update 1.1.1

--- script/socialweb.js
+++ script/socialweb.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name       Social Web Tampermonkey Script
-// @namespace  https://github.com/vitrvm/SocialWeb
-// @version    0.1.1
-// @description  Make the WWW the social web
-// @match      *://*/*
-// @updateURL  https://raw.githubusercontent.com/vitrvm/SocialWeb/main/script/socialweb.js
+// @name        Social Web Tampermonkey Script
+// @namespace   https://github.com/vitrvm/SocialWeb
+// @version     0.1.1
+// @description Make the WWW the social web
+// @match       *://*/*
+// @downloadURL https://raw.githubusercontent.com/vitrvm/SocialWeb/main/script/socialweb.js
+// @updateURL   https://raw.githubusercontent.com/vitrvm/SocialWeb/main/script/socialweb.js
 // @noframes
-// @copyright  2022+ Frank Garrote Cruz
+// @copyright   2022+ Frank Garrote Cruz
 // ==/UserScript==
 
 var body = document.body;
